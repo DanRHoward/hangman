236c74274ade27d561e9240dfb48ebb4babe1ad5
DanRHoward <116043233+DanRHoward@users.noreply.github.com>
2022-10-19 14:20:50 +0100

    Adding validation mechanic for player input

--- milestone_2.py
+++ milestone_2.py
@@ -7,3 +7,11 @@ word = random.choice(word_list)
 print(word)
 
 guess = input('Which letter will you guess? : ')
+
+i = 0
+while i != 1:
+    if len(guess) == 1:
+        if guess.isalpha() == True:
+            print('Good guess!'); i += 1 
+        else: print('Oops! That is not a valid input.'); guess = input('Try again : ')
+    else: print('Oops! That is not a valid input.'); guess = input('Try again : ')
